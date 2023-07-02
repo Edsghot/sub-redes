ca0c1b5e861cdc8a133988b9c309dc5b713e2f36
Ed Soto <edsghot@gmail.com>
2023-07-01 19:15:24 -0500

    Agregando los input y la validacion de la clase

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,107 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { normalizeClass, ref } from "vue";
+const ip1 = ref(0);
+const ip2 = ref(0);
+const ip3 = ref(0);
+const ip4 = ref(0);
+const numSubRedes = ref(0);
+const claseIP = ref(" ");
+const quito = ref(0)
+
+const generar = () => {
+  verificarClase();
+
+  claseIP.value = "La clase IP: "+claseIP.value
+};
+
+const verificarClase=()=>{
+  if(ip1.value<128 && ip1.value>0){
+    claseIP.value="A"
+  }else if(ip1.value<192 && ip1.value >127){
+    claseIP.value="B"
+  }else if(ip1.value<224 && ip1.value>191){
+    claseIP.value="C"
+  }else if(ip1.value<226 && ip1.value>223){
+    claseIP.value="A"
+  }else{
+    claseIP.value="INVALIDA"
+  }
+}
+
+
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <div class="container">
+    <form onsubmit="event.preventDefault();">
+      <div class="form-group">
+        <h4 class="mt-2">Digite su IP:</h4>
+        <div class="row">
+          <div class="col-1">
+            <input
+              type="number"
+              class="form-control form-control-sm rounded col-1"
+              v-model="ip1"
+            />
+          </div>
+          <div class="col-1">
+            <input
+              type="number"
+              class="form-control form-control-sm rounded col-1"
+              v-model="ip2"
+            />
+          </div>
+          <div class="col-1">
+            <input
+              type="number"
+              class="form-control form-control-sm rounded col-1"
+              v-model="ip3"
+            />
+          </div>
+          <div class="col-1">
+            <input
+              type="number"
+              class="form-control form-control-sm rounded col-1"
+              v-model="ip4"
+            />
+          </div>
+        </div>
+        <h2 class="mt-3">{{ ip1 }}.{{ ip2 }}.{{ ip3 }}.{{ ip4 }}</h2>
+      </div>
+      <div class="form-group">
+        <h4>Digite la cantidad de subredes que desea:</h4>
+        <div class="col-1">
+          <input
+            type="number"
+            v-model="numSubRedes"
+            class="form-control form-control-sm rounded col-1"
+          />
+        </div>
+        <h4>QUITO:</h4>
+        <div class="col-1">
+          <input
+            type="number"
+            v-model="quito"
+            class="form-control form-control-sm rounded col-1"
+          />
+        </div>
+      </div>
+      <br />
+      <button type="submit" @click="generar" class="btn btn-primary">Generar</button>
+    </form>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+    <h3>{{ claseIP }}</h3>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
+<style>
+body {
+  background-color: grey;
 }
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.input-custom-size {
+  padding: 0.375rem 0.75rem;
+  font-size: 1rem;
+  height: 2.25rem;
 }
 </style>
